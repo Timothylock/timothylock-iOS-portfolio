1fd45cfda6c21c8eb02788f0ceba800c78aa809a
Timothy Lock <lock.k.timothy@gmail.com>
2015-04-17 13:13:11 -0400

    Finish Education
    Added and finished education and added navigation bar to connect

--- Timothy Lock/WebViewController.swift
+++ Timothy Lock/WebViewController.swift
@@ -11,17 +11,49 @@ import UIKit
 class WebViewController: UIViewController  {
     
     @IBOutlet var webView: UIWebView!
+    @IBOutlet var goForward: UIBarButtonItem!
+    @IBOutlet var goBack: UIBarButtonItem!
+    @IBOutlet var stop: UIBarButtonItem!
+    @IBOutlet var reload: UIBarButtonItem!
+
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Load the webpage
         let url = NSURL(string: "http://timothylock.ca/#connect")
         let request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
     }
     
+    @IBAction func openURLSafari(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://timothylock.ca/")!)
+
+    }
+
+    @IBAction func doRefresh(AnyObject) {
+        // Refresh Webpage
+        webView.reload()
+    }
+    
+    @IBAction func goBack(AnyObject) {
+        // Go Back
+        webView.goBack()
+    }
+    
+    @IBAction func goForward(AnyObject) {
+        // Go forward
+        webView.goForward()
+    }
+    
+    @IBAction func stop(AnyObject) {
+        // Stop Loading
+        webView.stopLoading()
+    }
     
     @IBAction func onBurger() {
+        // Open Menu
         (tabBarController as! TabBarController).sidebar.showInViewController(self, animated: true)
     }
     
